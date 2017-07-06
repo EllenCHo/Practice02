579fa27878efd825eb1a2191a552ee8aa5c72c5a
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 14:21:06 +0900

    Problem02 finish

---
+++ src/com/javaex/prac02/Problem02.java
@@ -0,0 +1,23 @@
+package com.javaex.prac02;
+
+import java.util.Scanner;
+
+public class Problem02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] nums = new int[5];
+		int sum = 0;
+		
+		for(int i =0; i<5; i++) {
+			nums[i] = sc.nextInt();
+			
+			sum += nums[i];
+		}
+		
+		double average = sum/5; 
+		
+		System.out.println("평균은 "+average+" 입니다.");
+	}
+
+}
