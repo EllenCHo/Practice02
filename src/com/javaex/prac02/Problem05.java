23247fe5aa4887d218660824c157ae5dccf48365
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 14:56:46 +0900

    Problem05  finish

---
+++ src/com/javaex/prac02/Problem05.java
@@ -0,0 +1,26 @@
+package com.javaex.prac02;
+
+public class Problem05 {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[6];
+		
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = (int)(Math.random()*45)+1;
+			
+			for(int j = 0; j<i; j++) {
+				if(numbers[j] == numbers[i] ) {
+					i--;
+					
+					break;
+				}
+				
+			}
+			
+			System.out.print(numbers[i]+"\t");
+			
+		}
+		
+	}
+
+}
