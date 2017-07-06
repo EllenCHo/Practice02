7d21ad6dbb6819d109f1df624e8fea540bbe424a
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 14:16:02 +0900

    Problem 1 finish

---
+++ src/com/javaex/prac02/Problem01.java
@@ -0,0 +1,23 @@
+package com.javaex.prac02;
+
+public class Problem01 {
+	public static void main(String[] args) {
+		int[] data = { 1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31 };
+		int num, sum;
+		sum = 0;				//배수의 합
+		num = 0;				//개수
+		
+		
+		for(int i =0; i<data.length; i++) {
+			if(data[i]%3==0) {
+				num++;
+				sum+=data[i];
+			}
+		}
+		
+		
+		System.out.println("주어진 배열에서 3의 배수의 개수->"+num);
+		System.out.println("주어진 배열에서 3의 배수의 합->"+sum);
+	}
+
+}
