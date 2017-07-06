05ad3166d4224e9c1ab147bd546768cf9fcf731b
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 15:15:26 +0900

    Problem04 향상된 for문을 이용

--- src/com/javaex/prac02/Problem04.java
+++ src/com/javaex/prac02/Problem04.java
@@ -2,6 +2,8 @@ package com.javaex.prac02;
 
 import java.util.Scanner;
 
+
+//이것이 자바다 책에서 나온 향상된 for문을 이용
 public class Problem04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,10 +14,10 @@ public class Problem04 {
 		int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
 		
 		
-		for(int i = 0; i<unit.length; i++) {
-			System.out.println(unit[i]+"원 : "+ money/unit[i]+"개");
+		for(int i : unit) {
+			System.out.println(i+"원 : "+ money/i+"개");
 			
-			money = money % unit[i];				//나눈 나머지값 다시 저장 
+			money = money % i;				//나눈 나머지값 다시 저장 
 													//그 단위 후의 단위로 다시 계산할 수 있음
 		}
 		
